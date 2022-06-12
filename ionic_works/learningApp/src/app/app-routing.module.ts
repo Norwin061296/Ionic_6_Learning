d0c5fd6745b48634534a12065687121a50ec98ef
Norwin <norwinnmw061296@hotmail.com>
2022-06-12 15:51:57 -0600

    new page in app

--- ionic_works/learningApp/src/app/app-routing.module.ts
+++ ionic_works/learningApp/src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'home-page',
+    loadChildren: () => import('./home-page/home-page.module').then( m => m.HomePagePageModule)
   }
 ];
 
